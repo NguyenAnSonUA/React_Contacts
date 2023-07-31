4594eeb42b216ffe71fdf64ad8a5744808e89a43
An Son NGUYEN <nguyen.ansonua@gmail.com>
2023-07-31 22:45:59 +0300

    add: initial files

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      Welcome to my contacts app. You can add, remove and edit your contacts.
+      <br />
+      Check out my{" "}
+      <a href="https://github.com">
+        github here.
+      </a>
+      .
+    </p>
+  );
+}
